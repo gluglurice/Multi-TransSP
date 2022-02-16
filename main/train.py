74fa99cbb8cdbbf112c1c6f6760efbfb33cf519a
gluglurice <gluglurice@qq.com>
2022-02-16 21:59:55 +0800

    train.py: Save the last model.

--- main/train.py
+++ main/train.py
@@ -143,12 +143,12 @@ def train():
                 if not os.path.exists(mc.model_path):
                     os.makedirs(mc.model_path)
                 torch.save(model.state_dict(), f'{mc.model_path}/epoch_{epoch + 1}.pth')
-            # if epoch == mc.epoch_end - 1:
-            #     """Reset min_loss for the next fold."""
-            #     mc.min_loss = 1e10
-            #     if not os.path.exists(mc.model_path):
-            #         os.makedirs(mc.model_path)
-            #     torch.save(model.state_dict(), f'{mc.model_path}/fold_{ki + 1}_epoch_{epoch + 1}.pth')
+            if epoch == mc.epoch_end - 1:
+                """Reset min_loss for the next fold."""
+                mc.min_loss = 1e10
+                if not os.path.exists(mc.model_path):
+                    os.makedirs(mc.model_path)
+                torch.save(model.state_dict(), f'{mc.model_path}/fold_{ki + 1}_epoch_{epoch + 1}.pth')
 
 
 if __name__ == '__main__':
