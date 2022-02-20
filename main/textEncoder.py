874ca86db89b556402ee86c73f58ae7ae6097a99
gluglurice <gluglurice@qq.com>
2022-02-20 17:06:28 +0800

    Modified textEncoder to fc.

--- main/textEncoder.py
+++ main/textEncoder.py
@@ -6,16 +6,25 @@ Author: Han
 import torch
 import torch.nn as nn
 
+import main.mainConfig as mc
+
 
 class TextEncoder(nn.Module):
     """
-    Linearly transforms text.
+    Encode text.
     """
     def __init__(self):
         super(TextEncoder, self).__init__()
-        self.weight = nn.Parameter(torch.rand(1))
-        self.bias = nn.Parameter(torch.rand(1))
+        self.fc = nn.Linear(mc.text_len, mc.text_len)
 
     def forward(self, x):
-        x = self.weight * x + self.bias
+        x = self.fc(x)
         return x
+
+
+if __name__ == '__main__':
+    # print(TextEncoder())
+    text_encoder = TextEncoder().to(mc.device)
+    text = torch.rand(1, mc.text_len, dtype=torch.float32).to(mc.device)
+    text_feature = text_encoder(text)
+    print(text_feature.shape)
