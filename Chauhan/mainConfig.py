2b3584cbb52989825f312d52af8bcf60a3105374
gluglurice <gluglurice@qq.com>
2022-02-23 19:23:18 +0800

    Adjusted lr of Yap and Chauhan.

--- Chauhan/mainConfig.py
+++ Chauhan/mainConfig.py
@@ -33,7 +33,7 @@ text_length_dim = 2     # text is of torch.Size((1, 1, 12)) and we take the 2nd 
 """(2) Network"""
 patient_batch_size = 1
 batch_size = 64
-lr = 1e-3
+lr = 1e-4
 weight_decay = 1e-6
 
 date_time = datetime.now().strftime("%Y%m%d%H%M%S")
